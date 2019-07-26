423d9bfa1b45c4a3b37c392ceacb62c33061b5c0
Nadia Dencheva <nadia.dencheva@gmail.com>
2019-07-26 11:23:21 -0400

    enable testing of schemas

--- conftest.py
+++ conftest.py
@@ -1,4 +1,12 @@
 import os
+import pkg_resources
+
+entry_points = []
+for entry_point in pkg_resources.iter_entry_points('pytest11'):
+    entry_points.append(entry_point.name)
+
+if "asdf_schema_tester" not in entry_points:
+    pytest_plugins = ['asdf.tests.schema_tester']
 
 # this contains imports plugins that configure py.test for astropy tests.
 # by importing them here in conftest.py they are discoverable by py.test
@@ -11,6 +19,7 @@ from astropy.tests.plugins.display import PYTEST_HEADER_MODULES, TESTED_VERSIONS
 # exceptions
 #enable_deprecations_as_exceptions()
 
+
 try:
     PYTEST_HEADER_MODULES['Astropy'] = 'astropy'
     PYTEST_HEADER_MODULES['asdf'] = 'asdf'
@@ -20,14 +29,10 @@ except (NameError, KeyError):
 
 # This is to figure out the affiliated package version, rather than
 # using Astropy's
-from . import version
+from gwcs import version
 
 try:
     packagename = os.path.basename(os.path.dirname(__file__))
     TESTED_VERSIONS[packagename] = version.version
 except NameError:   # Needed to support Astropy <= 1.0.0
     pass
-
-pytest_plugins = [
-    'asdf.tests.schema_tester'
-]
